aae3814393fee19d9dc4f0eb32bddfe9527980d5
arif cahyady <arifcihoy@gmail.com>
2020-08-20 06:51:23 +0700

    inisial commit

---
+++ sprint-1/topik-2/latihan-1.php
@@ -0,0 +1,22 @@
+<?php  
+
+//Program Perhitungan Bangun Ruang
+
+//Deklarasi
+$panjang = 5;
+$lebar = 8;
+$hasil_perhitungan = $panjang * $lebar;
+
+echo "Luas Persegi Panjang";
+echo "Panjang = 5<br>";
+echo "Lebar = 8<br>";
+echo "Luas = Panjang * Lebar<br>";
+echo "=====================<br>";
+echo "Hasil adalah: " . $hasil_perhitungan;
+
+
+
+
+
+
+?>
