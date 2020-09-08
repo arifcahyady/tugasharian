7e7a6212b68f9fe3e18f1b5c6c8f2cc738faad33
arif cahyady <arifcihoy@gmail.com>
2020-09-08 21:29:39 +0700

    inisial commit

---
+++ sprint-1/topik-3/Belajarlooping/latihan.php
@@ -0,0 +1,65 @@
+<?php  
+														// latihan pola 1
+
+// $n = 9;
+// for ($i=1; $i <= $n ; $i++) { 
+// 	for ($j=1; $j <= $n ; $j++) { 
+// 		if ($i==$j) {
+// 			echo "+";
+
+// 		}elseif ($i>$j) {
+// 			echo "+";
+// 		}elseif ($i<$j) {
+// 			echo "-";
+// 		}else {
+// 			echo "-";
+// 		}		
+// 	}
+// 	echo "\n";;
+// }
+
+														//latihan pola 2
+
+// for ($i=1; $i < 10 ; $i++) { 
+// 	for ($n=9; $n>0 ; $n--) { 
+// 		if ($i>=$n) {
+// 			echo "+";
+// 		} else {
+// 			echo "-";
+// 		}
+// 	}
+// 	echo "\n";
+// }
+
+														// latihan pola 3
+
+	// $n = 9;
+	// for ($i=0; $i < $n; $i++) { 
+	// 	for ($j=0; $j < $n ; $j++) { 
+	// 		if (($i + $j) <= 8 and $i <= $j) {
+	// 			echo "+ ";
+	// 		}elseif (($i + $j) <= 16 and ($i + $j) > 7 and $i >= $j) {
+	// 			echo "+ ";
+	// 		}else {
+	// 			echo "- ";
+	// 		}
+	// 	}
+	// 	echo "\n";
+	// }
+
+	// $n = 9;
+	// for ($i=0; $i < $n ; $i++) { 
+	// 	for ($j=0; $j < $n ; $j++) { 
+	// 		if ($i == 0 or $i == 4 or $i == 8) {
+	// 			echo "+ ";
+	// 		}elseif ($i <= 3 and $j == 0) {
+	// 			echo "+ ";
+	// 		}elseif ($i >= 5 and $j == 8) {
+	// 			echo "+ ";
+	// 		}else {
+	// 			echo "- ";
+	// 		}
+	// 	}
+	// 	echo "\n";
+	// }
+?>
