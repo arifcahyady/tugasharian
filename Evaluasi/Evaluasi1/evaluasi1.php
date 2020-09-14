ca4e2f222b4a3cb91f95b6c56a2357cc72e009f9
arif cahyady <arifcihoy@gmail.com>
2020-09-14 17:17:58 +0700

    inisial commit

--- Evaluasi/Evaluasi1/evaluasi1.php
+++ Evaluasi/Evaluasi1/evaluasi1.php
@@ -1,86 +1,90 @@
 <?php 
 
-// for ($i=0; $i < 10; $i++) { 
-// 	for ($j=0; $j < 10; $j++) { 
-// 		echo "(".$i . $j .")";
-// 	}
-// 	echo "<br>";
-// }
-
-
-// echo "<br>";
-// for ($i=0; $i < 10; $i++) { 
-// 	for ($j=0; $j < 10; $j++) { 
-// 		if ($j == 0) {
-// 			echo "+" . "\n";
-// 		}elseif ($j == 2) {
-// 			echo "+". "\n";
-// 		}elseif ($j == 4) {
-// 			echo "+". "\n";
-// 		}elseif ($j == 6) {
-// 			echo "+". "\n";
-// 		}elseif ($j == 8) {
-// 			echo "+". "\n";
-// 		}else{
-// 			echo "-";
-// 		}
-// 	}
-// 	echo "<br>";
-// }
-
-
-
-
-// echo "<br>";
-// for ($i=0; $i < 10; $i++) { 
-// 	for ($j=0; $j < 10; $j++) { 
-// 		if ($i == 0) {
-// 			echo "-" . "\n";
-// 		}elseif ($i == 2) {
-// 			echo "-". "\n";
-// 		}elseif ($i == 4) {
-// 			echo "-". "\n";
-// 		}elseif ($i == 6) {
-// 			echo "-". "\n";
-// 		}elseif ($i == 8) {
-// 			echo "-". "\n";
-// 		}else{
-// 			echo "+";
-// 		}
-// 	}
-// 	echo "<br>";
-// }
-
-// $angka = [1,2,3,4,5,6];
-
-// function values($value) {
-// 	global $angka;
-
-// 	return $value == $angka;
-// 	// $angka = int;
-
-	
-// } 
-// echo  values([1,2,3,4,5,0,6]);
-// print_r($angka);	
-
-echo "======================================"."\n";
-echo "| Selamat Datang Di Program Input Data|"."\n";
-echo "======================================="."\n";
-	
-function inputdata() {
-	echo "nama ; ";
-	$nama = trim(fgets(STDIN));
-	echo "nik : ";
-	$nik = trim(fgets(STDIN));
-	echo "jurusan : ";
-	$jurusan = trim(fgets(STDIN));
-	echo "divisi : ";
-	$divisi = trim(fgets(STDIN));
-	echo "usia : ";
-	$usia = trim(fgets(STDIN));
-
-
+for ($i=1; $i <= 9 ; $i++) { 
+	for ($k=1; $k <= 9; $k++) { 
+		echo "(". $i,$k . ")";
+	}
+echo "\n";
 }
 
-inputdata();	
+echo "\n";
+for ($i=1; $i <= 9; $i++) { 
+	for ($k=1; $k <= 9; $k++) { 
+		if ($k == 1 || $k == 3 || $k == 5 || $k == 7 || $k == 9) {
+			echo "+";
+		}else {
+			echo "-";
+		}
+	}
+echo "\n";
+}
+
+echo "\n";
+for ($i=1; $i <= 9; $i++) { 
+	for ($k=1; $k <= 9; $k++) { 
+		if ($i == 1 || $i == 3 || $i == 5 || $i == 7 || $i == 9) {
+			echo "-";
+		}else {
+			echo "+";
+		}
+	}
+echo "\n";
+}
+
+$a = 5956560159466056;
+
+$explode = explode(0, $a);
+// print_r($explode);
+
+	foreach ($explode as $key => $value) {
+		$result[] = str_split($explode[$key]);
+		sort($result[$key]);
+	}
+
+$hasil = array_merge(...$result);
+// print_r($result);
+echo "Hasil nya : ";
+foreach ($hasil as $get) {
+	echo $get;
+}
+
+// echo "=================================="."\n";
+// echo "||	Program input data         ||" . "\n";
+// echo "==================================" . "\n";
+// echo "\n";
+
+// echo "Mau input berapa user : ";
+// $input = trim(fgets(STDIN));
+
+// 	for ($i=1; $i <= $input ; $i++) { 
+// 		echo "data ke : " .$i . "\n";
+// 		echo "Masukan user : ";
+// 		$nama = trim(fgets(STDIN));
+// 		echo "Masukan umur : ";
+// 		$umur = trim(fgets(STDIN));
+// 		echo "Masukan jurusan : ";
+// 		$jurusan = trim(fgets(STDIN));
+// 		echo "Masukan divisi : ";
+// 		$divisi = trim(fgets(STDIN));
+	
+
+// 	$hasil = [
+// 		'nama' => $nama,
+// 		'umur' => $umur,
+// 		'jurusan' => $jurusan,
+// 		'divisi' => $divisi
+// 	];
+
+
+// // $hasil[];
+
+	
+// };
+// print_r($hasil);
+ 
+// foreach ($hasil as $k) {
+// 	if ($k['divisi'] == 'backend') {
+// 		$get = $k['nama'];
+// 	}		
+// }
+// print_r($get);
