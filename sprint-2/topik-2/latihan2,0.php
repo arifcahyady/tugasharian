e6dfd0f552ac3b5864f09bd5509440e3842050df
arif cahyady <arifcihoy@gmail.com>
2020-09-21 17:04:34 +0700

    initial commit

--- sprint-2/topik-2/latihan2,0.php
+++ sprint-2/topik-2/latihan2,0.php
@@ -1,17 +1,10 @@
 <?php  
 include_once 'class2,0.php';
-
-echo "Hasil hitung luas persegi adalah : ";
-$hasil = new HitungBangunPersegi(2,5);
+echo "Luas Bangun Persegi adalah : ";
+HitungBangunPersegi::luasPersegi($panjang,$tinggi);
 echo "\n";
-echo "Hasil hitung keliling persegi adalah : ";
-$hasil = new HitungBangunPersegi(2,7 * 2);
-echo "\n";
-echo "Hasil hitung luas lingkaran adalah : ";
-$hasil = new HitungBangunLingkaran(3.14,5);
-echo "\n";
-echo "Hasil hitung keliling lingkaran adalah : ";
-$hasil = new HitungBangunLingkaran(3,14, 5 / 2 * 2);
+echo "Luas Bangun Lingkaran adalah : ";
+HitungBangunLingkaran::luasLingkaran($phi,$diameter);
 echo "\n";
 echo "Hasil hitung luas trapesium adalah : ";
 $hasil = new HitungBangunTrapesium(2,5 / 2);
