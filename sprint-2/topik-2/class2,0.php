3cbdab7ae61edc47ec3eefeb26c46245294b9431
arif cahyady <arifcihoy@gmail.com>
2020-09-22 10:53:15 +0700

    change commit

--- sprint-2/topik-2/class2,0.php
+++ sprint-2/topik-2/class2,0.php
@@ -1,34 +1,30 @@
 <?php 
+ $panjang = 12;
+ $tinggi = 21;
+ $phi = 3.14;
+ $diameter = 12;
 
 class HitungBangunPersegi
 {
-	// public $panjang;
-	// public $tinggi;
-
-	public function __construct($panjang, $tinggi) {
+	public static function luasPersegi($panjang, $tinggi) {
 		echo $panjang * $tinggi;
-	}
 
+	}
 }
 class HitungBangunLingkaran
 {
 
-	public function __construct($phi, $diameter) {
+	public static function luasLingkaran($phi, $diameter) {
 		echo $phi * $diameter;
 	}
-
-	
 }
 
 
 
 class HitungBangunTrapesium
 {
-	
-
 	public function __construct($rusuk, $tinggi) {
 		echo $rusuk * $tinggi;
-	}
-
-	
+	}	
 }
+
