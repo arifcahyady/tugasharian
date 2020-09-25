219cb13ce1cf99db8efa0821059bf2abc63e8537
arif cahyady <arifcihoy@gmail.com>
2020-09-25 13:47:49 +0700

    evaluasi 3

---
+++ Evaluasi/Evaluasi3/data_santri.sql
@@ -0,0 +1,53 @@
+-- MariaDB dump 10.17  Distrib 10.4.13-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: nilai_santri
+-- ------------------------------------------------------
+-- Server version	10.4.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `santri`
+--
+
+DROP TABLE IF EXISTS `santri`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `santri` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(30) DEFAULT NULL,
+  `divisi` varchar(30) DEFAULT NULL,
+  `nilai` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `santri`
+--
+
+LOCK TABLES `santri` WRITE;
+/*!40000 ALTER TABLE `santri` DISABLE KEYS */;
+INSERT INTO `santri` VALUES (1,'andi',NULL,58),(2,'lala',NULL,60),(3,'lulu',NULL,60),(4,'lili',NULL,60),(5,'loli',NULL,64),(6,'randi',NULL,65),(7,'ranui',NULL,66),(8,'randu',NULL,66),(9,'randa',NULL,69),(10,'fanny',NULL,70),(11,'grock',NULL,70),(12,'aldous',NULL,72),(13,'alice',NULL,73),(14,'clint',NULL,75),(15,'lesley',NULL,75),(16,'harley',NULL,75),(17,'yi shun shin',NULL,80),(18,'vale',NULL,89),(19,'valir',NULL,90),(20,'arif',NULL,95);
+/*!40000 ALTER TABLE `santri` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-25 13:43:07
