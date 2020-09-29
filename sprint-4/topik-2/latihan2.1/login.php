3fd2d5fd0c8c07db363a82c1e828f24e6a94642d
arif cahyady <arifcihoy@gmail.com>
2020-09-29 16:45:53 +0700

    inisial commit

--- sprint-4/topik-2/latihan2.1/login.php
+++ sprint-4/topik-2/latihan2.1/login.php
@@ -9,8 +9,9 @@ $error = '';
 
 if (isset($_POST['submit'])) {
     	$nama = $_POST['nama'];
-    	$password = $_POST['password'];
-    if (empty($nama) && empty($password)) {
+        $password = $_POST['password'];
+    	$email = $_POST['email'];
+    if (empty($nama) && empty($password) && empty($email)) {
          $error = "Username dan Password salah";         
     }else {
         $_SESSION['nama'] = $nama;
@@ -25,17 +26,23 @@ if (isset($_POST['submit'])) {
 <head>
     <title>Login Sederhana Tanpa Database</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="login.css">
 </head>
-<body>
- 
-    <h3>Silakan Login</h3>
- 
-    <form method="POST" action="login.php">
-        Nama : <input type="text" name="nama">
-        Password : <input type="password" name="password">
-        <input type="submit" name="submit" value="Login"><br>
-        <?php echo $error; ?>
-    </form>
-    
+<body class="container-fluid">
+ <div class="main_content">
+     <div class="card text-center rounded-lg" style="width: 35em; height: 30em;background-color: #EEEDED;">
+        <div class="card-body"> 
+            <h3 class="card-title mb-5">Silahkan Login</h3>
+            <form method="POST" action="login.php">
+                <input type="text" name="nama" placeholder="Nama" class="card-text mb-4" style="width: 26em; height: 40px;"><br>
+                <input type="password" name="password" placeholder="Password" class="card-text mb-4" style="width: 26em; height: 40px;"><br>
+                <input type="email" name="email" placeholder="E-mail" class="card-text mb-5" style="width: 26em; height: 40px;"><br>
+                <input type="submit" name="submit" value="Login" class="btn btn-primary mb-5"><br>
+                <a href="#">Create an account</a><br>
+                <?php echo $error; ?>
+            </form>
+        </div>
+    </div>
+</div> 
 </body>
 </html>
